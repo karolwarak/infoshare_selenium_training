8bac5be58c8e49ea8581b82e4d39057fbaa9a7d5
Robert Kaszubowski <robertkaszubowski@MacBook-Pro-Robert.local>
2018-01-07 13:08:48 +0100

    JS and screenshot

--- src/test/java/basics/Module2_SeleniumFeatures.java
+++ src/test/java/basics/Module2_SeleniumFeatures.java
@@ -1,16 +1,16 @@
 package basics;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import utils.driver.WebDriverCreators;
 import utils.driver.WebDriverProvider;
 import utils.waits.CustomWait;
 
+import java.io.File;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -19,7 +19,7 @@ public class Module2_SeleniumFeatures {
 
     private final static String PAGE_HEADER_TEXT = "Selenium: Beginners Guide";
     private final static String CONFIRMATION_BOX_MESSAGE = "This is double click";
-
+    private final static String SCREENSHOT_FILE_PATH = "Screenshot/screenshot.png";
 
     private WebDriver driver;
 
@@ -70,6 +70,35 @@ public class Module2_SeleniumFeatures {
         customWait.waitForDocumentDisappear(refreshedListOfDocuments);
 
         assertEquals("Document has not been deleted.", 3, refreshedListOfDocuments.size());
+    }
+
+    @Test
+    public void javaScriptCallTest() {
+        driver.get("http://www.google.com");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String title = (String) js.executeScript("return document.title");
+
+        assertEquals("Google", title);
+
+        long links = (Long) js.executeScript("var links = document.getElementsByTagName ('A'); return links.length");
+
+        assertEquals("Wrong number of links.",40, links);
+    }
+
+    @Test
+    public void screenShotTest() {
+        driver.get("http://book.theautomatedtester.co.uk/chapter1");
+        WebElement pageHeader = driver.findElement(By.xpath("//div[@class= 'mainheading']"));
+
+        try {
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File(SCREENSHOT_FILE_PATH));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertEquals("Page header is incorrect.", PAGE_HEADER_TEXT, pageHeader.getText());
     }
 
     @After
